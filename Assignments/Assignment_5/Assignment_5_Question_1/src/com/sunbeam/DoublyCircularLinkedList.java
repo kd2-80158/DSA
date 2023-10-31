8b8210d60f718008f5c60fe7ddd85fa91232f860
kd2-80158 <chhimwalsaurabh@gmail.com>
2023-10-31 23:36:50 +0530

    Assignment 5 question-1 uploaded

---
+++ Assignments/Assignment_5/Assignment_5_Question_1/src/com/sunbeam/DoublyCircularLinkedList.java
@@ -0,0 +1,112 @@
+package com.sunbeam;
+
+public class DoublyCircularLinkedList {
+	
+	class Node
+	{
+		private int data;
+		private Node prev;
+		private Node next;
+		
+		public Node(int value)
+		{
+			data=value;
+			prev=null;
+			next=null;
+		}
+	}
+	private Node head;
+	
+	public DoublyCircularLinkedList()
+	{
+		head = null;
+	}
+    
+	public boolean isEmpty()
+	{
+		return head==null;
+	}
+	//to avoid invalid positions like pos less than 1 or pos greater than count(number of elements)
+	int count=0;
+public int addPosition(int value,int pos)
+{
+	
+	Node newnode = new Node(value);
+	
+	if(isEmpty())
+	{
+		head=newnode;
+		head.prev=newnode;
+		head.next=newnode;
+		count++;
+		
+	}
+	else
+	{
+		Node trav = head;
+		for(int i=1;i<pos;i++)
+		{
+			trav=trav.next;
+			
+		}
+		newnode.prev=trav.prev;
+		newnode.next=trav;
+		//first add this to avoid data loss
+		trav.prev.next=newnode;
+		trav.prev=newnode;
+		count++;
+		System.out.println("count: "+count);
+	}
+	return count;
+}
+public void deletePosition(int pos)
+{
+	if(isEmpty())
+	{
+		System.out.println("List is empty");
+	}
+	else if(pos>count || pos<1)
+	{
+		return;
+	}
+	else if(pos==count)
+	{
+		Node trav = head;
+		for(int i=1;i<pos-1;i++)
+		{
+			trav=trav.next;
+		}
+		trav.next=trav.prev;
+		trav.prev.prev=trav;
+		
+	}
+	else
+	{
+		Node trav = head;
+		for(int i=1;i<pos-1;i++)
+		{
+			trav=trav.next;
+		}
+		trav.next=trav.next.next;
+		trav.next.prev=trav;
+		
+	}
+}
+
+public void display()
+{
+	if(isEmpty())
+	{
+		System.out.println("List is empty");
+	}
+	Node trav = head;
+		
+		do
+		{
+			System.out.println(" "+trav.data);
+			trav=trav.next;
+		}
+		while(trav!=head);
+}
+
+}
---
+++ Assignments/Assignment_5/Assignment_5_Question_1/src/com/sunbeam/MainClass.java
@@ -0,0 +1,24 @@
+package com.sunbeam;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		DoublyCircularLinkedList list = new DoublyCircularLinkedList();
+		
+		list.addPosition(10, 1);
+		list.addPosition(20, 2);
+		list.addPosition(30, 3);
+		list.addPosition(40, 4);
+		
+//		slist.deletePosition(3);
+		list.deletePosition(3);
+//		list.deletePosition(4);
+		
+		list.display();
+		
+		
+
+	}
+
+}
